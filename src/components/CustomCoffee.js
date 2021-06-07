f017f18b510bd564ba9fc766358c622a1e6b56d9
Ivan <miltonivan117@gmail.com>
2021-06-07 07:34:57 -0600

    add more ingredients

--- src/components/CustomCoffee.js
+++ src/components/CustomCoffee.js
@@ -4,8 +4,11 @@ export const CustomCoffee = () => {
     const [ingredient, setIngredient] = useState({
         sugar:false,
         milk:false,
+        cafeinapura: false,
         cold:false,
-        chocolat:false,
+        hot: false,
+        crema:false,
+        hot: false,
     })
     //
     const handleSendCoffee = () => {
@@ -22,15 +25,42 @@ export const CustomCoffee = () => {
         <div>
             <h1>Coffee</h1>
             <p>Customize your coffee with</p>
-            <p>Sugar</p>
+            <p>Azucar</p>
             <input onChange={handleCheck} checked={ingredient.sugar}  name="sugar" type="checkbox" />
-            <p >Milk</p>
+            <p >Leche</p>
             <input onChange={handleCheck} checked={ingredient.milk}  name="milk" type="checkbox" />
-            <p >Chocolat</p>
-            <input onChange={handleCheck}  checked={ingredient.chocolat} name="chocolat" type="checkbox" />
+            <p >Crema</p>
+            <input onChange={handleCheck}  checked={ingredient.crema} name="crema" type="checkbox" />
+            <p>Cafeina pura</p>
+            <input onChange={handleCheck}  checked={ingredient.cafeinapura} name="cafeinapura" type="checkbox" />
             <p>Cold</p>
             <input onChange={handleCheck}  checked={ingredient.cold} name="cold" type="checkbox" />
+            <p>Hot</p>
+            <input onChange={handleCheck}  checked={ingredient.hot} name="hot" type="checkbox" />
             <button onClick={handleSendCoffee}>Send</button>
         </div>
     )
 }
+/*
+cafe latte__
+*leche
+*sugar
+*expreso
+*frio o caliente
+
+cafe capuccino___
+*leche
+*espreso
+*sugar
+*frio o caliente
+
+espresso
+cafeina normal
+
+americano__
+*frio o caliente
+*Crema
+*cafeina puro
+
+
+*/
