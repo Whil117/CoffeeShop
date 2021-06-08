a51b4196c351bd541e0467a9d1708c1b15ca66ef
Ivan <miltonivan117@gmail.com>
2021-06-07 18:08:52 -0600

    add system orders

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,16 @@
+import Head from 'next/head'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { CustomCoffee } from "../components/CustomCoffee"
 export default function Home() {
   return (
     <>
-      <h1>Custom Coffee</h1>
       <CustomCoffee />
     </>
   )
